facf33d5b17f54a04b5662f96256c037dfd6249e
San-15 <49862h10@gmail.com>
2021-05-23 02:11:52 +0530

    Production configuration peer 1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const pubsub = new PubSub({blockchain,transactionPool,wallet});
 const transactionMiner = new TransactionMiner({blockchain,transactionPool,wallet,pubsub});
 
 const DEFAULT_PORT=3000;
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+const ROOT_NODE_ADDRESS = isDevelopment ?  `http://localhost:${DEFAULT_PORT}` : 'http://desolate-cliffs-84797.herokuapp.com';
 
 setTimeout(() => pubsub.broadcastChain(),1000);
 
